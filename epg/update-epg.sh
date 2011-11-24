f132162040bdea49c9f464bfd86cc2a8032a98ba
Guillaume DELVIT <guiguid@free.fr>
2011-11-24 09:49:23 +0100

    first commit

---
+++ epg/update-epg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /var/lib/vdr/epg/
+rm /var/lib/vdr/epg/tnt.zip
+rm /var/lib/vdr/epg/tnt.xml
+wget http://xmltv.dyndns.org/download/tnt.zip
+unzip /var/lib/vdr/epg/tnt.zip
+/usr/bin/perl /var/lib/vdr/epg/xmltv2vdr.pl -t 900 -v -x /var/lib/vdr/epg/tnt.xml -c /var/lib/vdr/epg/channels.conf
+
